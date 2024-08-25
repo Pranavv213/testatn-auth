62d8b14c585deae8923eb07171f6786df78d9529
new <new@Pranavs-Mac-mini.local>
2024-08-25 23:49:22 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,60 @@
+import React,{useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
+import { db } from "./firebase-config.js";
+import {
+  collection,
+  getDocs,
+  addDoc,
+  updateDoc,
+  deleteDoc,
+  doc,
+} from "firebase/firestore";
+const userCollection = collection(db, "user");
 
 function App() {
+  const [username,setUsername]=useState('')
+  const [code,setCode]=useState('')
+  const [msg,setMsg]=useState('')
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <br></br><br></br><br></br><br></br><br></br><br></br>
+      <br></br><br></br><br></br><br></br><br></br><br></br><br></br>
+     <input placeholder='Enter Telegram Username' onChange={(e)=>{
+      setUsername(e.target.value)
+     }}></input>
+     <br></br>
+     <br></br>
+     <input placeholder='Enter the Code sent on the Bot' onChange={(e)=>{
+      setCode(e.target.value)
+     }}></input>
+       <br></br>
+       <br></br>
+     <button onClick={async()=>{
+       const data = await getDocs(userCollection);
+     
+       let dbdata= data.docs.map((doc) => ({ ...doc.data(), id: doc.id }));
+  
+       for(let i=0;i<dbdata.length;i++)
+          {
+             
+              if(dbdata[i].username==username)
+                  {
+                      if (dbdata[i].otp==code)
+                      {
+                         setMsg('User Logged In')
+                      }
+                  }
+              else{
+                  setMsg('Wrong Code. Please Enter Correct Code')
+              }
+              }
+             
+     }}>Let's Go</button>
+     <br></br>
+     <br></br>
+     {msg}
     </div>
   );
 }
